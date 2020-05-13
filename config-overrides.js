f5be0cb798a3007ec2a3fa3353f136b5a99078df
wangning <ning.wang@ifchange.com>
2020-05-13 18:02:54 +0800

    基本解构大家

--- config-overrides.js
+++ config-overrides.js
@@ -5,6 +5,10 @@ module.exports = function override(config, env) {
   config.resolve.alias = {
     ...config.resolve.alias,
     "@src": path.join(__dirname, "./src"),
+    "@assets": path.join(__dirname, "./src/assets"),
+    "@pages": path.join(__dirname, "./src/pages"),
+    "@components": path.join(__dirname, "./src/components"),
+    "@utils": path.join(__dirname, "./src/utils"),
   };
   return config;
 };
---
+++ src/pages/index/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default () => {
+  return <div>This Is Index</div>;
+};
